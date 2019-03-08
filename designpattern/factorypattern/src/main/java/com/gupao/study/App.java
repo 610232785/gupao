2b1373cf1f780c51d207ba90daf6c9174844b77b
610232785 <610232785@qq.com>
2019-03-08 09:27:45 +0800

    无

--- designpattern/factorypattern/src/main/java/com/gupao/study/App.java
+++ designpattern/factorypattern/src/main/java/com/gupao/study/App.java
@@ -2,12 +2,11 @@ package com.gupao.study;
 
 /**
  * Hello world!
- *
+ * @author rainly
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
